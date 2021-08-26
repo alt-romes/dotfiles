179f85f5c9151cee4e401a92f51f39059bb3c90e
romes <rodrigo.m.mesquita@gmail.com>
2021-08-26 20:20:29 +0100

    Update iTerm profiles and update setup

--- setup.sh
+++ setup.sh
@@ -10,12 +10,25 @@ then
 	HERE=$(pwd)
 
 	# create dotfiles symlinks
-	cd $HOME
-	ln -si ${HERE}/.bash_profile
-	ln -si ${HERE}/.gitconfig
-	ln -si ${HERE}/.gitignore_global
-	ln -sFi ${HERE}/.vim/
-	ln -sFi ${HERE}/.iterm/
+	cd "$HOME" || exit 1
+	ln -si "${HERE}/.bash_profile" .
+	ln -si "${HERE}/.gitconfig" .
+	ln -si "${HERE}/.gitignore_global" .
+	ln -sFi "${HERE}/.vim/" .
+	ln -sFi "${HERE}/.iterm/" .
 
-	cd ${HERE}
+	cd "${HERE}" || exit 1
+fi
+
+read -p "Overwrite iTerm preferences? (y/n) " -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp -i .iterm/com.googlecode.iterm2.plist "$HOME/Library/Preferences/"
+
+    echo
+    echo "Important => Required manual actions:"
+    echo "  iTerm: Go to 'Preferences > General > Preferences'"
+    echo "  iTerm: Select 'Load preferences from a custom folder or URL'"
+    echo "  iTerm: Input '~/.iterm'"
+    echo "  iTerm: Note, if a required font isn't installed, some profiles might revert to the default font"
 fi
