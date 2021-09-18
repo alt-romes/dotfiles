38d1b1b34a9051c4459f0c02e30d136e760fc61c
romes <rodrigo.m.mesquita@gmail.com>
2021-09-18 18:37:39 +0100

    setup openbox pywal theme

--- setup.sh
+++ setup.sh
@@ -24,8 +24,6 @@ then
     ln -sFi "${HERE}/.config/gtk-3.0/" ./.config/
     
     ln -sFi "${HERE}/.themes/" .
-    # extract openbox themes to .themes
-    find "${HERE}.themes/openbox-theme-collections/" -maxdepth 1 -type d -exec cp -R -t "${HERE}/.themes/" {} +
 
     ln -sFi "${HERE}/.icons/" .
 
@@ -35,6 +33,35 @@ then
 
 	cd "${HERE}" || exit 1
 fi
+
+read -p "Run pywal once and install extensions (e.g. for openbox)? (y/n) " -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	HERE=$(pwd)
+
+    mkdir -p "$HOME/Pictures/backgrounds"
+    echo "Move at least one background image to ~/Pictures/backgrounds"
+    echo "(Example: curl -L https://i.imgur.com/COg47x2.png > ~/Pictures/backgrounds/b1.png)"
+    echo "(... https://i.imgur.com/KlhtVEY.png > ~/Pictures/backgrounds/b2.png)"
+    read -r -p "Input any key afterwards... "
+
+    echo "Installing walbox (pywal + openbox)"
+    cd build-dependencies/walbox/
+    echo "Copying user template to .config/wal/templates/"
+    mkdir -p "$HOME/.config/wal/templates/"
+    cp -i themerc "$HOME/.config/wal/templates/"
+    echo "Copying openbox pywal theme to ~/.themes"
+    cp -ri Walbox "$HOME/.themes/Walbox"
+    echo "Creating symlink in openbox theme to pywal user template generated theme (theme dependency)"
+    ln -s "$HOME/.cache/wal/themerc" "$HOME/.themes/Walbox/openbox-3/themerc"
+
+    cd "${HERE}" || exit 1
+
+    echo "TODO: pywal must be run on a terminal emulator rather than in the tty console"
+    echo "Running pywal once (generating pywal files)"
+    wal --saturate 0.88 -i "$HOME/Pictures/backgrounds"
+fi
+
 
 read -p "Overwrite iTerm preferences? (y/n) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
