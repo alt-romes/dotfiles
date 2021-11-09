26fba339d35de4fca90007a38428e3c84ba00116
romes <rodrigo.m.mesquita@gmail.com>
2021-11-09 00:45:02 +0000

    separate linux/darwin, and pywal from openbox confirmation scope

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+HERE=$(pwd)
+
 echo "Setting up public dotfiles..."
 
 echo "On creating symlinks:"
@@ -7,69 +9,79 @@ echo "This may overwrite existing files in your home directory."
 read -p "Create symlinks? (y/n) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	HERE=$(pwd)
 
 	# create dotfiles symlinks
-	cd "$HOME" || exit 1
-	ln -si "${HERE}/.bash_profile" .
-	ln -si "${HERE}/.gitconfig" .
-	ln -si "${HERE}/.gitignore_global" .
-	ln -sFi "${HERE}/.vim/" .
-	ln -sFi "${HERE}/.emacs.d/" .
-	ln -sFi "${HERE}/.iterm/" .
+	ln -si "${HERE}/.bash_profile" "$HOME"
+	ln -si "${HERE}/.gitconfig" "$HOME"
+	ln -si "${HERE}/.gitignore_global" "$HOME"
+	ln -sFi "${HERE}/.vim/" "$HOME"
+	ln -sFi "${HERE}/.emacs.d/" "$HOME"
+	ln -sFi "${HERE}/.iterm/" "$HOME"
 
-    echo "TODO: Linux only?"
-    mkdir -p .config
-    ln -sFi "${HERE}/.config/openbox/" ./.config/
-    ln -si "${HERE}/.config/picom.conf" ./.config/
-    ln -sFi "${HERE}/.config/tint2/" ./.config/
-    ln -sFi "${HERE}/.config/sway/" ./.config/
+    # linux specific dotfiles
+    if [[ $(uname) == "Linux" ]]
+    then
+        mkdir -p "$HOME/config"
+        ln -sFi "${HERE}/.config/openbox/" "$HOME/.config/"
+        ln -si "${HERE}/.config/picom.conf" "$HOME/.config/"
+        ln -sFi "${HERE}/.config/tint2/" "$HOME/.config/"
+        ln -sFi "${HERE}/.config/sway/" "$HOME/.config/"
 
-    ln -si "${HERE}/.xinitrc" .
-    ln -si "${HERE}/.Xresources" .
+        ln -si "${HERE}/.xinitrc" "$HOME"
+        ln -si "${HERE}/.Xresources" "$HOME"
+    fi
 
-
-	cd "${HERE}" || exit 1
 fi
 
-read -p "Run pywal once and install extensions (e.g. for openbox)? (y/n) " -r
+read -p "Run pywal once? (y/n) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	HERE=$(pwd)
 
     mkdir -p "$HOME/Pictures/backgrounds"
     echo "Move at least one background image to ~/Pictures/backgrounds"
     echo "(Example: curl -L https://i.imgur.com/V2eZQPm.jpeg > ~/Pictures/backgrounds/b1.jpeg)"
     read -r -p "Input any key afterwards... "
 
-    echo "Installing walbox (pywal + openbox)"
-    cd build-dependencies/walbox/
-    echo "Copying user template to .config/wal/templates/"
-    mkdir -p "$HOME/.config/wal/templates/"
-    cp -i themerc "$HOME/.config/wal/templates/"
-    echo "Copying openbox pywal theme to ~/.themes"
-    mkdir -p "$HOME/.themes/"
-    cp -ri Walbox "$HOME/.themes/Walbox"
-    echo "Creating symlink in openbox theme to pywal user template generated theme (theme dependency)"
-    ln -s "$HOME/.cache/wal/themerc" "$HOME/.themes/Walbox/openbox-3/themerc"
-
-    cd "${HERE}" || exit 1
-
-    echo "TODO: pywal must be run on a terminal emulator rather than in the tty console"
     echo "Running pywal once (generating pywal files)"
     wal --saturate 0.88 -i "$HOME/Pictures/backgrounds"
+
 fi
 
-
-read -p "Overwrite iTerm preferences? (y/n) " -r
-if [[ $REPLY =~ ^[Yy]$ ]]
+# Linux only
+if [[ $(uname) == "Linux" ]]
 then
-    cp -i .iterm/com.googlecode.iterm2.plist "$HOME/Library/Preferences/"
+    read -p "Install walbox -- openbox pywal extension? (y/n) " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
 
-    echo
-    echo "Important => Required manual actions:"
-    echo "  iTerm: Go to 'Preferences > General > Preferences'"
-    echo "  iTerm: Select 'Load preferences from a custom folder or URL'"
-    echo "  iTerm: Input '~/.iterm'"
-    echo "  iTerm: Note, if a required font isn't installed, some profiles might revert to the default font"
+        echo "Installing walbox (pywal + openbox)"
+        cd build-dependencies/walbox/ || exit 1
+        echo "Copying user template to .config/wal/templates/"
+        mkdir -p "$HOME/.config/wal/templates/"
+        cp -i themerc "$HOME/.config/wal/templates/"
+        echo "Copying openbox pywal theme to ~/.themes"
+        mkdir -p "$HOME/.themes/"
+        cp -ri Walbox "$HOME/.themes/Walbox"
+        echo "Creating symlink in openbox theme to pywal user template generated theme (theme dependency)"
+        ln -s "$HOME/.cache/wal/themerc" "$HOME/.themes/Walbox/openbox-3/themerc"
+
+    fi
+fi
+
+# macOS only
+if [[ $(uname) == "Darwin" ]]
+then
+    read -p "Overwrite iTerm preferences? (y/n) " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "TODO: Change this to a soft link?"
+        cp -i .iterm/com.googlecode.iterm2.plist "$HOME/Library/Preferences/"
+
+        echo
+        echo "Important => Required manual actions:"
+        echo "  iTerm: Go to 'Preferences > General > Preferences'"
+        echo "  iTerm: Select 'Load preferences from a custom folder or URL'"
+        echo "  iTerm: Input '~/.iterm'"
+        echo "  iTerm: Note, if a required font isn't installed, some profiles might revert to the default font"
+    fi
 fi
