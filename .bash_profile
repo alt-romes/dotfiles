ace9bc7d5f6b45565579aca56770ba4ba686c13b
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-08-10 14:08:18 +0100

    Update public dotfiles

--- .bash_profile
+++ .bash_profile
@@ -6,13 +6,21 @@
 # +-----------------------------------*/
 
 # $PATH
-# export PATH="/usr/local/sbin:$PATH" # Homebrew's sbin
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-export PATH="/usr/local/opt/openjdk/bin:$PATH" # Java
+eval "$(/opt/homebrew/bin/brew shellenv)" # Set PATH, MANPATH, etc., for Homebrew.
+export PATH="/opt/homebrew/opt/llvm@15/bin:$PATH"
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH" # Java
+export PATH="/opt/homebrew/opt/sphinx-doc/bin:$PATH" # sphinx-docs
 export PATH="$HOME/.cabal/bin:/Users/romes/.ghcup/bin:$PATH" # Haskell Platform
 export PATH="$HOME/.cargo/bin:$PATH" # Rust Platform
 export PATH="$HOME/.local/bin:$PATH" # .local/bin
 export PATH="$HOME/control/util/bin:$PATH" # Control scripts
+export PATH="$HOME/ghc-dev/ghc-utils:$PATH" # ghc-util scripts, e.g. ghc-docker-jobs.sh aarch64-linux-deb10-validate
+export PATH="$HOME/.opam/CP.2023.03+b1~8.17~2023.03+beta1/bin:$PATH"
+
+# Library path
+export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+# Include path
+export CPATH="$CPATH:$(brew --prefix)/include"
 
 # JAVA
 export JAVA_HOME="$(/usr/libexec/java_home 2> /dev/null)"
@@ -22,7 +30,7 @@ alias java-versions="/usr/libexec/java_home -V"
 # $PS1
 function setps1() {
     white="\[\[\033[0m\]"
-    colors=("\[\033[01;31m\]" "\[\033[01;32m\]" "\[\033[01;33m\]" "\[\033[01;34m\]" "\[\033[01;35m\]" "\[\033[01;36m\]" "\[\033[01;37m\]")
+    colors=("\[\033[01;31m\]" "\[\033[01;32m\]" "\[\033[01;33m\]" "\[\033[01;34m\]" "\[\033[01;35m\]" "\[\033[01;36m\]")
     kaomoji=("(・_・)ノ" "(^_^♪)" "(>_<)" "(o^ ^o)" "(„• ᴗ •„)" "(๑˃ᴗ˂)ﻭ" "(*^.^*)" "ヾ(๑╹◡╹)ﾉ\"" "（╹◡╹）♡ " "(๑╹ω╹๑ )" "ᕦ(ò_óˇ)ᕤ " "Σ(-᷅_-᷄๑)" "(ง'̀-'́)ง" "ʕ•ᴥ•ʔ" "(>^.^<)" "⚑")
     randomcolor=${colors[$((RANDOM % 7))]}
     export PS1="${randomcolor}(${white}\W${randomcolor}) ${kaomoji[$((RANDOM % 16))]}${white} "
@@ -33,6 +41,13 @@ setps1
 alias mv='mv -i'
 alias cp='cp -i'
 
+# vim + fzf
+alias vif='vim "$(fzf)"'
+# fzf <== ripgrep
+# since we already use ripgrep, we might as well use by default with fzf.
+# (by default, `find`)
+export FZF_DEFAULT_COMMAND='rg --files --ignore-vcs --hidden'
+
 # don't put duplicates in history
 HISTCONTROL=ignoredups
 
@@ -40,12 +55,13 @@ HISTCONTROL=ignoredups
 export EDITOR="vim"
 
 # load bash completion
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 
 # use wal colorscheme
 if [[ -f ~/.cache/wal/sequences ]]
 then
-    (cat ~/.cache/wal/sequences &)
+    # (cat ~/.cache/wal/sequences &)
+    true
 fi
 
 # +----oOO-{&}-OOo---------------------+
@@ -58,11 +74,7 @@ alias maek="make"
 alias mak="make"
 alias mkae="make"
 
-function fastpush() {
-    git add .
-    git commit -m "$1"
-    git push
-}
+alias g="git"
 
 alias clear='[ $[$RANDOM % 5] = 0 ] && gtimeout 6 cbeams -o; clear || clear'
 alias matrix='perl -e "$|++; while (1) { print \" \" x (rand(35) + 1), int(rand(2)) }"'
@@ -102,12 +114,16 @@ alias walf="wal -f"
 
 alias anime="ani-cli"
 alias httpserver="python -m http.server 25565"
+alias python="$(brew --prefix)/bin/python3"
 
 alias ghc-shell="nix-shell ~/ghc-dev/ghc.nix/"
 
 alias ghci-core="ghci -ddump-simpl -dsuppress-idinfo \
 -dsuppress-coercions -dsuppress-type-applications \
 -dsuppress-uniques -dsuppress-module-prefixes"
+
+
+alias docker-ghc-linux-deb10="~/ghc-dev/ghc-utils/ghc-docker-jobs.sh aarch64-linux-deb10-validate"
 
 
 if [[ $(uname) == "Darwin" ]]
@@ -136,3 +152,20 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# Append always to history
+# and increase its size
+PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
+HISTSIZE=5000
+HISTFILESIZE=5000
+
+
+. "$HOME/.cargo/env"
+
+# Enable/disable coreutils installed by homebrew
+export PATH="$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix)/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$PATH"
+
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,167 +4,53 @@
 
 ;; Set up package.el to work with MELPA
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (package-initialize)
-(unless package-archive-contents
-    (package-refresh-contents))
+(unless package-archive-contents (package-refresh-contents))
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-    (package-install 'evil))
-
-;; Enable Evil
+;; Evil
+(unless (package-installed-p 'evil) (package-install 'evil))
 (require 'evil)
 (evil-mode 1)
 
-;; Download Twilight-Bright Theme
-(unless (package-installed-p 'twilight-bright-theme)
-    (package-install 'twilight-bright-theme))
-(load-theme 'twilight-bright t)
+;; org-roam
+(unless (package-installed-p 'org-roam) (package-install 'org-roam))
 
 ;; Polymode
-(unless (package-installed-p 'polymode)
-    (package-install 'polymode))
-
-;; Disable weird background highlight in vue-mode
-(add-hook 'mmm-mode-hook
-    (lambda ()
-        (set-face-background 'mmm-default-submode-face nil)))
+(unless (package-installed-p 'polymode) (package-install 'polymode))
 
 ;; Customize user interface.
-; (menu-bar-mode 0)
-(when (display-graphic-p)
-    (tool-bar-mode 0)
-    (scroll-bar-mode 0))
+; (menu-bar-mode 0)??
+(when (display-graphic-p) (tool-bar-mode 0) (scroll-bar-mode 0))
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+; (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
-;;; Emfy 0.1.0-dev <https://github.com/susam/emfy>
+;; Buffer encoding
+;(prefer-coding-system       'utf-8)
+;(set-default-coding-systems 'utf-8)
+;(set-terminal-coding-system 'utf-8)
+;(set-keyboard-coding-system 'utf-8)
+;(set-language-environment   'utf-8)
 
-;; Customize user interface.
-; (menu-bar-mode 0)
-; (setq inhibit-startup-screen t)
-; (column-number-mode)
-
-; ;; Theme.
-; (load-theme 'wombat)
-; (set-face-background 'default "#111")
-; (set-face-background 'cursor "#c96")
-; (set-face-background 'isearch "#c60")
-; (set-face-foreground 'isearch "#eee")
-; (set-face-background 'lazy-highlight "#960")
-; (set-face-foreground 'lazy-highlight "#ccc")
-; (set-face-foreground 'font-lock-comment-face "#fc0")
-
-; ;; Interactively do things.
-; (ido-mode 1)
-; (ido-everywhere)
-; (setq ido-enable-flex-matching t)
-
-; ;; Show stray whitespace.
-; (setq-default show-trailing-whitespace t)
-; (setq-default indicate-empty-lines t)
-; (setq-default indicate-buffer-boundaries 'left)
-
-; ;; Consider a period followed by a single space to be end of sentence.
-; (setq sentence-end-double-space nil)
-
-; ;; Use spaces, not tabs, for indentation.
-; (setq-default indent-tabs-mode nil)
-
-; ;; Display the distance between two tab stops as 4 characters wide.
-; (setq-default tab-width 4)
-
-; ;; Indentation setting for various languages.
-; (setq c-basic-offset 4)
-; (setq js-indent-level 2)
-; (setq css-indent-offset 2)
-
-; ;; Highlight matching pairs of parentheses.
-; (setq show-paren-delay 0)
-; (show-paren-mode)
-
-; ;; Write auto-saves and backups to separate directory.
-; (make-directory "~/.emacs.d/backup/" t)
-; (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
-; (setq backup-directory-alist '(("." . "~/.emacs.d/backup/margo/")))
-
-; ;; Disable lockfiles.
-; (setq create-lockfiles nil)
-
-; ;; Workaround for https://debbugs.gnu.org/34341 in GNU Emacs <= 26.3.
-; (when (and (version< emacs-version "26.3") (>= libgnutls-version 30603))
-;   (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
-; ;; Write customizations to ~/.emacs.d/custom.el instead of this file.
-; (setq custom-file "~/.emacs.d/custom.el")
-
-; ;; Install packages.
-; (setq package-list '(markdown-mode paredit rainbow-delimiters))
-; (dolist (package package-list)
-;   (unless (package-installed-p package)
-;     (package-install package)))
-
-; ;; Enable Paredit.
-; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-; (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-
-; ;; Enable Rainbow Delimiters.
-; (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-; (add-hook 'ielm-mode-hook 'rainbow-delimiters-mode)
-; (add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
-; (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-
-; ;; Customize Rainbow Delimiters.
-; (require 'rainbow-delimiters)
-; (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
-; (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
-; (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
-; (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
-; (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
-; (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
-; (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
-; (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
-; (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
-
-; ;; Custom command.
-; (defun show-current-time ()
-;   "Show current time for 2 seconds."
-;   (interactive)
-;   (message (current-time-string))
-;   (sleep-for 2)
-;   (message nil))
-
-; ;; Custom key-binding.
-; (global-set-key (kbd "C-c t") 'show-current-time)
-
-; ;; Start server.
-; (require 'server)
-; (unless (server-running-p)
-;   (server-start))
+;; Font
+; (add-to-list 'default-frame-alist '(font . "Iosevka" ))
 
 ;; Ewal
-; (unless (package-installed-p 'ewal)
-;     (package-install 'ewal))
+; (unless (package-installed-p 'ewal) (package-install 'ewal))
 
 ; (setq ewal-built-in-palette "sexy-material")
 
-;; Agda?
-; (load-file (let ((coding-system-for-read 'utf-8))
-;                 (shell-command-to-string "agda-mode locate")))
+; Agda Mode
+;; To use Unicode from \latex expressions, press <C-\> in insert mode
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" "e7ba99d0f4c93b9c5ca0a3f795c155fa29361927cadb99cfce301caf96055dfd" default))
- '(package-selected-packages
-   '(haskell-mode haskell-emacs exotica-theme twilight-theme ## p vue-mode twilight-bright-theme rainbow-delimiters paredit markdown-mode ewal evil)))
+ '(package-selected-packages '(polymode org-roam evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
